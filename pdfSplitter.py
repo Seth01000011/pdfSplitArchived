6a8d3f97c147f7fe5ade54c75a175c6e121f689e
seth01000011 <seth01000011@gmail.com>
2022-10-27 00:16:00 -0400

    PDF Search working

--- pdfSplitter.py
+++ pdfSplitter.py
@@ -1,8 +1,19 @@
-import PyPDF2
-import os
+from PyPDF2 import PdfReader
+import os, sys
 
-input_pdf = open('02meetingminutes2.pdf', 'rb')
-read_input_pdf = PyPDF2.PdfFileReader(input_pdf);
-print(read_input_pdf.numPages)
-page = read_input_pdf.getPage(0)
-print(page.extractText())
+if len(sys.argv) < 2:
+  raise ValueError("Usage: Type filename.pdf for the pdf you want to split")
+if len(sys.argv) < 3:
+  raise ValueError("Please enter the keyword you are searching for")
+
+keyword = sys.argv[2]
+input_pdf = PdfReader(sys.argv[1])
+page_breaks = []
+for x in range(len(input_pdf.pages)):
+  page = input_pdf.pages[x]
+  print("Page " + str(x))
+  text = page.extract_text()
+  if keyword in text:
+    print("Found on page " + str(x))
+    page_breaks.append(x)
+print(page_breaks)
